c8a0e160cda1eff58058278b111f19597a482037
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-06-28 11:12:10 -0400

    Add favicons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Navbar from "./components/Navbar";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
