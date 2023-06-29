d41e0ade12037edc6d8f4a12a2f7ad832ca68486
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-06-29 18:22:31 -0400

    Set-up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import Navigation from "./components/Navigation/Navigation";
-import "./App.css";
+// Bootstrap Styles
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// Components
+import Navigation from "./components/Navigation/Navigation";
 import Homepage from "./components/HomepageBody/Homepage";
 import Footer from "./components/Footer/Footer";
 
+// Custom Designs
+import "./App.css";
+
+// React Router dom
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
     return (
-        <div className="App">
-            <Navigation />
-            <Homepage />
-            <Footer />
-        </div>
+        <Router>
+            <div className="App">
+                <Navigation />
+                <Switch>
+                    <Route exact path="/">
+                        <Homepage />
+                    </Route>
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
