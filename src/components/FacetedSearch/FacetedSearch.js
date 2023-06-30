f225a86702fea41451386f3500af4975d9631032
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-06-29 20:18:22 -0400

    Faceted search initial files

---
+++ src/components/FacetedSearch/FacetedSearch.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+const FacetedSearch = () => {
+    return (
+        <Container className="my-5">
+            <h2 class="py-5 heading-secondary text-center">
+                SHOP MODERN ISLAMIC ART
+            </h2>
+        </Container>
+    );
+};
+
+export default FacetedSearch;
