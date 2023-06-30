26b9ea36ef12ae6af5420b8f2773a111b0fd9fdb
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-06-30 15:19:54 -0400

    Update filter logic

--- src/components/FacetedSearch/FacetedSearch.js
+++ src/components/FacetedSearch/FacetedSearch.js
@@ -1,97 +1,29 @@
-import {
-    Container,
-    DropdownButton,
-    ButtonGroup,
-    ListGroup,
-} from "react-bootstrap";
+import { Container, Badge, CloseButton } from "react-bootstrap";
+import Dropdowns from "./Dropdowns";
 import "./styles.css";
+import { useState } from "react";
 
 const FacetedSearch = () => {
+    const [appliedFilters, setAppliedFilters] = useState(null);
+
     return (
         <Container className="my-5">
             <h2 className="py-5 heading-secondary text-center">
                 SHOP MODERN ISLAMIC ART
             </h2>
-            <AllDropdowns />
+            <Dropdowns
+                filtersApplied={appliedFilters}
+                editFilters={setAppliedFilters}
+            />
             <hr className="hr my-5 py-5" />
-        </Container>
-    );
-};
-
-const AllDropdowns = () => {
-    return (
-        <div className="container d-flex gap-3">
-            <p className="heading-tertiary fs-5 fw-normal m-0">Filter:</p>
-            <div className="container row g-4 row-cols-3 row-cols-md-5">
-                <FilterDropdown
-                    title="Category"
-                    filters={["Calligraphy", "Architecture", "Geometric"]}
-                    className="col rounded-0"
-                />
-                <FilterDropdown
-                    title="Content"
-                    filters={[
-                        "Islamic Verses",
-                        "Sayings",
-                        "Popular Duas",
-                        "Vegetal Patterns",
-                        "Geometric Shapes",
-                        "Middle Eastern Architecture",
-                        "Asian Architecture",
-                    ]}
-                    className="col"
-                />
-                <FilterDropdown
-                    title="Sets"
-                    filters={["Single", "Dual", "Triple", "Quad"]}
-                    className="col"
-                />
-                <FilterDropdown
-                    title="Colors"
-                    filters={[
-                        "Red",
-                        "Green",
-                        "Blue",
-                        "Yellow",
-                        "Orange",
-                        "Black",
-                    ]}
-                    className="col"
-                />
-                <FilterDropdown
-                    title="Price"
-                    filters={["<$50", "$50 - $100", "$200+"]}
-                    className="col"
-                />
+            <div>
+                <p>Your Filters: </p>
+                <Badge pill bg="dark">
+                    Dark
+                    <CloseButton variant="white" />
+                </Badge>
             </div>
-        </div>
-    );
-};
-
-const FilterDropdown = ({ title, filters }) => {
-    return (
-        <DropdownButton
-            id="dropdown-basic-button"
-            className="list-group bg-dark-outline my-dropdown mb-4"
-            style={{ fontFamily: "Raleway,sans-serif" }}
-            title={title}
-            as={ButtonGroup}
-            variant="dark"
-            autoClose="outside"
-        >
-            <ListGroup variant="flush">
-                {filters.map((filter) => (
-                    <ListGroup.Item className="d-flex gap-3" eventKey={filter}>
-                        <input
-                            className="form-check-input flex-shrink-0"
-                            type="checkbox"
-                            value={filter}
-                        />
-                        {filter}
-                    </ListGroup.Item>
-                ))}
-            </ListGroup>
-        </DropdownButton>
+        </Container>
     );
 };
 
