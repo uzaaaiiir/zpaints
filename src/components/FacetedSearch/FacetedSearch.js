6c08a16f7fb497e82979cb3199e0ff833a753565
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-06-29 21:30:35 -0400

    Categories for faceted search

--- src/components/FacetedSearch/FacetedSearch.js
+++ src/components/FacetedSearch/FacetedSearch.js
@@ -13,50 +13,50 @@ const FacetedSearch = () => {
             <h2 className="py-5 heading-secondary text-center">
                 SHOP MODERN ISLAMIC ART
             </h2>
-            <div className="container row row-cols-1 row-cols-lg-6 align-items-center">
-                <p className="heading-tertiary fs-4 fw-normal mb-0">Filter:</p>
-                <FilterDropdown
-                    title="Category"
-                    filters={["Calligraphy", "Architecture", "Geometric"]}
-                    className="col rounded-0"
-                />
-                <FilterDropdown
-                    title="Content"
-                    filters={[
-                        "Islamic Verses",
-                        "Sayings",
-                        "Popular Duas",
-                        "Vegetal Patterns",
-                        "Geometric Shapes",
-                        "Middle Eastern Architecture",
-                        "Asian Architecture",
-                    ]}
-                    className="col"
-                />
-                <FilterDropdown
-                    title="Sets"
-                    filters={["Single", "Dual", "Triple", "Quad"]}
-                    className="col"
-                />
-                <FilterDropdown
-                    title="Colors"
-                    filters={[
-                        "Red",
-                        "Green",
-                        "Blue",
-                        "Yellow",
-                        "Orange",
-                        "Black",
-                    ]}
-                    className="col"
-                />
-                <FilterDropdown
-                    title="Price"
-                    filters={["<$50", "$50 - $100", "$200+"]}
-                    className="col"
-                />
-            </div>
+            <AllDropdowns />
+            <hr className="hr my-5 py-5" />
         </Container>
+    );
+};
+
+const AllDropdowns = () => {
+    return (
+        <div className="container row g-2 row-cols-3 row-cols-lg-6 align-items-center justify-content-center">
+            <p className="heading-tertiary fs-5 fw-normal m-0">Filter:</p>
+            <FilterDropdown
+                title="Category"
+                filters={["Calligraphy", "Architecture", "Geometric"]}
+                className="col rounded-0"
+            />
+            <FilterDropdown
+                title="Content"
+                filters={[
+                    "Islamic Verses",
+                    "Sayings",
+                    "Popular Duas",
+                    "Vegetal Patterns",
+                    "Geometric Shapes",
+                    "Middle Eastern Architecture",
+                    "Asian Architecture",
+                ]}
+                className="col"
+            />
+            <FilterDropdown
+                title="Sets"
+                filters={["Single", "Dual", "Triple", "Quad"]}
+                className="col"
+            />
+            <FilterDropdown
+                title="Colors"
+                filters={["Red", "Green", "Blue", "Yellow", "Orange", "Black"]}
+                className="col"
+            />
+            <FilterDropdown
+                title="Price"
+                filters={["<$50", "$50 - $100", "$200+"]}
+                className="col"
+            />
+        </div>
     );
 };
 
@@ -64,7 +64,7 @@ const FilterDropdown = ({ title, filters }) => {
     return (
         <DropdownButton
             id="dropdown-basic-button"
-            className="list-group bg-dark-outline my-dropdown"
+            className="list-group bg-dark-outline my-dropdown mb-4"
             style={{ fontFamily: "Raleway,sans-serif" }}
             title={title}
             as={ButtonGroup}
