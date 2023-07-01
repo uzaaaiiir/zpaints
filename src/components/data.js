272e84a340122055d4d6d2749720f551c8a84b2d
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-07-01 19:07:32 -0400

    Add data for the art results

---
+++ src/components/data.js
@@ -0,0 +1,33 @@
+import badshahiMosque from "../assets/img/display-paintings/architecture/asia-badshahi-mosque.png";
+import kaabahArchitecture from "../assets/img/display-paintings/architecture/kaaba-painting.png";
+import badshahiMosque1 from "../assets/img/display-paintings/architecture/badshahi-mosque.png";
+
+export const data = [
+    {
+        title: "Badshahi Mosque",
+        img: badshahiMosque,
+        price: 45.0,
+        categories: ["Architecture", "Asian Architecture"],
+        ratings: 4.7,
+        numOfReviews: 243,
+        id: 0,
+    },
+    {
+        title: "Kaabah Architecture",
+        img: kaabahArchitecture,
+        price: 55.0,
+        categories: ["Architecture", "Middle Eastern Architecture"],
+        rating: 5,
+        numOfReviews: 453,
+        id: 1,
+    },
+    {
+        title: "Badshahi Mosque",
+        img: badshahiMosque1,
+        price: 55.0,
+        categories: ["Architecture", "Middle Eastern Architecture"],
+        rating: 5,
+        numOfReviews: 453,
+        id: 2,
+    },
+];
