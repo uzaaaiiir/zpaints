fa63b7bc77d32f9b15fe79ed3f43621ee950fe20
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-07-22 14:12:13 -0400

    Initial file for custom design component

---
+++ src/components/CustomDesign/CustomDesign.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+const CustomDesign = () => {
+    return (
+        <Container className="my-5">
+            <h2 className="py-5 heading-secondary text-center">
+                Request a Custom Design
+            </h2>
+        </Container>
+    );
+};
+
+export default CustomDesign;
