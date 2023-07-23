cdbfb35ec36f4387099207f511dc760b083ab58d
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-07-22 21:17:56 -0400

    Complete functional User Details page of questionnaire

--- src/components/CustomDesign/CustomDesignPages/UserDetails.js
+++ src/components/CustomDesign/CustomDesignPages/UserDetails.js
@@ -2,21 +2,37 @@ import { Container, Button, Form, Col, Row } from "react-bootstrap";
 import { BiArrowBack } from "react-icons/bi";
 import { useNavigate } from "react-router-dom";
 
-const UserDetails = ({ onButtonClick }) => {
+const UserDetails = ({ onButtonClick, handleFormData, data }) => {
     const navigate = useNavigate();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        onButtonClick("pagetwo");
+    };
 
     return (
         <Container>
-            <Form style={{ fontFamily: "Raleway, sans-serif" }}>
-                <Row className="mb-3 w-75 text-start">
+            <h2
+                className="pt-3 py-3 mb-2 text-center font-weight-bold"
+                style={{ fontFamily: "Playfair Display, sans-serif" }}
+            >
+                User Details
+            </h2>
+            <Form
+                style={{ fontFamily: "Raleway, sans-serif" }}
+                onSubmit={(e) => onSubmit(e)}
+            >
+                <Row className="mb-4 py-2 mx-auto w-75 text-start">
                     <Form.Group as={Col}>
                         <Form.Label>First Name</Form.Label>
                         <Form.Control
                             required
                             type="text"
                             placeholder="First name"
+                            defaultValue={data.firstName}
                             autoFocus
                             className="rounded-0"
+                            onChange={handleFormData("firstName")}
                         />
                     </Form.Group>
                     <Form.Group as={Col}>
@@ -24,12 +40,14 @@ const UserDetails = ({ onButtonClick }) => {
                         <Form.Control
                             required
                             placeholder="Last name"
+                            defaultValue={data.lastName}
                             type="text"
                             className="rounded-0"
+                            onChange={handleFormData("lastName")}
                         />
                     </Form.Group>
                 </Row>
-                <Row className="mb-3 w-75 text-start">
+                <Row className="mb-4 py-2 mx-auto w-75 text-start">
                     <Form.Group
                         as={Col}
                         className="mb-3 w-75"
@@ -37,10 +55,13 @@ const UserDetails = ({ onButtonClick }) => {
                     >
                         <Form.Label>Phone Number</Form.Label>
                         <Form.Control
-                            placeholder="647-557-1021"
-                            type="tel"
                             required
+                            placeholder="647-557-1021"
+                            defaultValue={data.phoneNumber}
+                            type="tel"
+                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                             className="rounded-0"
+                            onChange={handleFormData("phoneNumber")}
                         />
                     </Form.Group>
                     <Form.Group
@@ -51,34 +72,34 @@ const UserDetails = ({ onButtonClick }) => {
                         <Form.Label>Email Address</Form.Label>
                         <Form.Control
                             placeholder="zpaints@gmail.com"
+                            defaultValue={data.email}
                             type="email"
                             required
                             className="rounded-0"
+                            onChange={handleFormData("email")}
                         />
                     </Form.Group>
                 </Row>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                <Container className="d-flex w-75 px-3 py-3 mb-4 justify-content-center align-items-center gap-5">
+                    <div
+                        className="d-flex align-items-center justify-content-center"
+                        style={{
+                            fontFamily: "Raleway, sans-serif",
+                            cursor: "pointer",
+                        }}
+                        onClick={() => navigate(-1)}
+                    >
+                        <BiArrowBack />
+                        <p className="mb-0 ps-2">Back</p>
+                    </div>
+                    <Button
+                        variant="btn btn-dark px-4 py-2 btn-style rounded-0"
+                        type="submit"
+                    >
+                        Next
+                    </Button>
+                </Container>
             </Form>
-
-            <Button
-                class="btn btn-dark btn-bg px-4 py-3 me-md-2 btn-style rounded-0"
-                style={{
-                    fontFamily: "Raleway, sans-serif",
-                }}
-                onClick={() => onButtonClick("pagetwo")}
-            >
-                Next
-            </Button>
-            <div
-                className="container d-flex align-items-center justify-content-start"
-                style={{ fontFamily: "Raleway, sans-serif", cursor: "pointer" }}
-                onClick={() => null}
-            >
-                <BiArrowBack />
-                <p className="mb-0 ps-2">Back</p>
-            </div>
         </Container>
     );
 };
