eb4db917fed79dd122b3b1087c12510142935283
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-07-23 11:14:40 -0400

    Files for internationalization

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from "./App";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// Import i18n
+import "./i18n";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
