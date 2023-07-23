0b522a6027267be2a91e47eb72c65a8edb8f68aa
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-07-22 21:16:12 -0400

    Add page that leads to questionnaire

--- src/components/CustomDesign/CustomDesign.js
+++ src/components/CustomDesign/CustomDesign.js
@@ -1,11 +1,38 @@
+import { Link } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import { useEffect } from "react";
 
 const CustomDesign = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <Container className="my-5">
             <h2 className="py-5 heading-secondary text-center">
                 Request a Custom Design
             </h2>
+
+            <p
+                className="my-3 mb-5 fs-5"
+                style={{ fontFamily: "Raleway, sans-serif" }}
+            >
+                Fill out the form below to answer a few questions about the
+                design are seeking. This will provide our artists a better
+                understanding of what you're looking for. Once you fill out the
+                form, we will get in touch within <strong>24 hours</strong>.
+                Click below to get started!
+            </p>
+
+            <Link
+                to="/questionnaire"
+                class="btn btn-dark px-4 py-3 mb-5 me-md-2 btn-style rounded-0"
+                style={{
+                    fontFamily: "Raleway, sans-serif",
+                }}
+            >
+                START QUESTIONNAIRE
+            </Link>
         </Container>
     );
 };
